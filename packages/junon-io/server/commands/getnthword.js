6b3f8808757b1487ee12014d71e8440e53f4ccfd
githubb-userr <119720721+githubb-userr@users.noreply.github.com>
2023-07-17 19:50:35 -0600

    Update and rename getsubstring.js to getnthword.js

---
+++ packages/junon-io/server/commands/getnthword.js
@@ -0,0 +1,35 @@
+const BaseCommand = require("./base_command")
+
+class GetNthWord extends BaseCommand {
+    getUsage() {
+        return [
+            "Returns a variable named $word",
+            "/getnthword [index] [message]",
+            "Ex: /getnthword 2 a random message, creates a $word variable, and assigns random to it."
+        ]
+    }
+    allowOwnerOnly() {
+        return true;
+    }
+    perform(caller,args) {
+        let index = args[0];
+        if(isNaN(index)) {
+            caller.showChatError(`index must be a number`)
+            return
+        }
+        let text = args.slice(1).join(" ")
+        
+        let stringArray = text.split(" ")
+
+        if(index > stringArray.length+1) {
+            caller.showChatError(`Invalid index`);
+            return;
+        }
+
+        this.game.executeCommand(this.sector,`/variable set word ${stringArray[index-1]}`)
+        caller.showChatSuccess('Success')
+    }
+    
+}
+
+module.exports = GetNthWord

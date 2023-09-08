bcb5555f7e7254bc9a549665e9369c071c603b98
githubb-userr <rachaelcodetesting@gmail.com>
2023-09-08 12:23:08 -0600

    Add dirt command

--- packages/junon-io/server/commands/index.js
+++ packages/junon-io/server/commands/index.js
@@ -66,6 +66,7 @@ Commands.wait = require("./wait")
 Commands.hour = require("./hour")
 Commands.day = require("./day")
 Commands.getnthword = require('./getnthword')
+Commands.dirt = require('./dirt')
 
 Commands.accept_rules = require("./accept_rules")
 

d395c8745c15386c2dc884e5494b36d9a1c3271e
kuroro <simple.yuji@gmail.com>
2024-01-16 17:10:51 -0500

    re-enable ads to cover server costs

--- packages/junon-io/client/src/entities/game.js
+++ packages/junon-io/client/src/entities/game.js
@@ -4260,6 +4260,9 @@ class Game {
     let shouldRefreshAd = !this.lastDeathAdDisplay ||
                           (this.lastDeathAdDisplay && ((Date.now() - this.lastDeathAdDisplay) > threeMinutes))
     if (shouldRefreshAd) {
+      aiptag.cmd.display.push(function() {
+        aipDisplayTag.display('junon-io_728x90');
+      })
       this.lastDeathAdDisplay = Date.now()
     }
   }

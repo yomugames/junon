01034b0043a67a053ce7da03d26d8f590b3e2200
MyExistenceIsAUser <elhackernoob888@gmail.com>
2024-10-01 23:55:31 -0300

    Update speed.js

--- packages/junon-io/server/commands/speed.js
+++ packages/junon-io/server/commands/speed.js
@@ -1,38 +1,54 @@
-const BaseCommand = require("./base_command")
-const Constants = require("../../common/constants")
-const Protocol = require('../../common/util/protocol')
+const BaseMob = require("./base_mob")
+const Protocol = require("./../../../common/util/protocol")
+const Constants = require("./../../../common/constants.json")
+const Projectiles = require('./../projectiles/index')
+const Mountable = require('../../interfaces/mountable')
 
-class Speed extends BaseCommand {
-  getUsage() {
-    return [
-      "/speed [player] [1-20]"
-    ]
+class Car extends BaseMob {
+  constructor(sector, data) {
+    super(sector, data)
+
+    this.initMountable()
+    this.previousSpeeds = new Map()
   }
 
-  allowOwnerOnly() {
-    return true
-  }
+  interact(user) {
+    if (!this.owner) return
+    if (!this.isOwnedBy(user)) return
 
-  perform(player, args) {
-    const selector = args[0]
-    const speed = parseInt(args[1])
+    if (!this.passenger) {
+      this.mobMount(user)
 
-    let targetPlayers = this.getPlayersBySelector(selector) 
-    if (targetPlayers.length === 0) {
-      player.showChatError("no players found")
-      return
+      this.previousSpeeds.set(user.id, user.speed)
+
+      user.speed = 12
+    } else if (this.passenger === user) {
+      this.mobUnmount()
+
+      const previousSpeed = this.previousSpeeds.get(user.id)
+      if (previousSpeed !== undefined) {
+        user.speed = previousSpeed
+        this.previousSpeeds.delete(user.id) // Clean up stored speed after restoration
+      }
     }
-
-    if (isNaN(speed) || speed < 0 || speed > 30) {
-      player.showChatError("/speed [1-30]")
-      return
-    }
-
-    targetPlayers.forEach((targetPlayer) => {
-      targetPlayer.speed = speed
-    })
   }
 
+  moveEntity(targetEntityToMove, deltaTime) {
+  }
+
+  canDamage(target) {
+    return false
+  }
+
+  getType() {
+    return Protocol.definition().MobType.Car
+  }
+
+  getConstantsTable() {
+    return "Mobs.Car"
+  }
 }
 
-module.exports = Speed
+Object.assign(Car.prototype, Mountable.prototype, {})
+
+module.exports = Car

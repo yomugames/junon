d25f0cca28058e2ed4ca9cc568b3712b52fafee8
chickemeeple <rachaelcodetesting@gmail.com>
2025-07-16 13:17:48 -0600

    Fix paralyzed being called W for no reason, giving hundreds of errors and causing lag buildup. Also added smoke to effect command description.

--- packages/junon-io/client/src/entities/statuses/index.js
+++ packages/junon-io/client/src/entities/statuses/index.js
@@ -7,7 +7,7 @@ Statuses.Thirst  = require("./thirst_status")
 Statuses.Stamina = require("./stamina_status")
 Statuses.Fear    = require("./fear_status")
 Statuses.Rage    = require("./rage_status")
-Statuses.W = require("./paralyze_status")
+Statuses.Paralyze = require("./paralyze_status")
 Statuses.Drunk = require("./drunk_status")
 Statuses.Miasma = require("./miasma_status")
 

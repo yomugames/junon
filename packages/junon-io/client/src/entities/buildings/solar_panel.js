216518af4fedb635b8da534d382fa1b56b632283
chickemeeple <rachaelcodetesting@gmail.com>
2025-07-21 11:23:12 -0600

    fix solar panel bug

--- packages/junon-io/client/src/entities/buildings/solar_panel.js
+++ packages/junon-io/client/src/entities/buildings/solar_panel.js
@@ -7,6 +7,7 @@ const Wire = require("./wire")
 class SolarPanel extends BaseBuilding {
 
   static isOnValidPlatform(container, x, y, w, h, angle, player) {
+    if(!super.isOnValidPlatform(container, x, y, w, h, angle, player)) return false; // base class prevents placing on negative coordinates
     if (this.isPlacingOnSomeoneElsePlatform(container, x, y, w, h, angle, player)) return false
 
     let box = this.getBox(x, y, w, h)

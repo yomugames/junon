d5cc018266fdc63c9c0293e17504c0925d009688
chickemeeple <rachaelcodetesting@gmail.com>
2025-07-29 12:02:57 -0600

    make basic server definition

---
+++ packages/junon-io/server/entities/mobs/villager.js
@@ -0,0 +1,24 @@
+const LandMob = require("./land_mob")
+const Protocol = require("./../../../common/util/protocol")
+const Constants = require("./../../../common/constants.json")
+const Projectiles = require('./../projectiles/index')
+
+class Villager extends LandMob {
+  getType() {
+    return Protocol.definition().MobType.Villager
+  }
+
+  getConstantsTable() {
+    return "Mobs.Villager"
+  }
+
+  canBeKnocked() {
+    return true
+  }
+
+  setNeutral(isNeutral) {
+    this.isNeutral = true // always neutral
+  }
+}
+
+module.exports = Slime

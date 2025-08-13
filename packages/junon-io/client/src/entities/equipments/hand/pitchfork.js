e918c2d3de5275e51ab593c0eeb31d527163f966
UnknownPerson-cmd <yoloman0416@gmail.com>
2025-08-13 02:56:21 +0000

    Pitchfork base art add later

---
+++ packages/junon-io/client/src/entities/equipments/hand/pitchfork.js
@@ -0,0 +1,61 @@
+const MeleeEquipment = require("./melee_equipment")
+const Protocol = require('../../../../common/util/protocol')
+const Constants = require("../../../../common/constants.json")
+
+class Pitchfork extends MeleeEquipment {
+  onEquipmentConstructed() {
+    // any setup if needed
+  }
+
+  getType() {
+    return Protocol.definition().BuildingType.Pitchfork
+  }
+
+  getConstantsTable() {
+    return "Equipments.Pitchfork"
+  }
+
+  use(player, targetEntity) {
+    if (targetEntity && targetEntity.isCrop && targetEntity.isCrop()) {
+      // interact with the crop (harvest/destroy fully grown)
+      if (targetEntity.isFullyGrown && targetEntity.isFullyGrown()) {
+        targetEntity.interact(player)
+        
+        // destroy left and right fully grown crops
+        this._destroyNeighbors(player, targetEntity)
+      }
+    } else {
+      // fallback to normal melee use
+      super.use(player, targetEntity)
+    }
+  }
+
+  _destroyNeighbors(player, target) {
+    const x = target.getX()
+    const y = target.getY()
+    const room = target.room
+
+    if (!room) return
+
+    const neighbors = [
+      room.getTile(x - 1, y),
+      room.getTile(x + 1, y)
+    ]
+
+    neighbors.forEach(tile => {
+      if (!tile) return
+
+      const crop = tile.getBuilding && tile.getBuilding()
+
+      if (crop && crop.isCrop && crop.isCrop() && crop.isFullyGrown && crop.isFullyGrown()) {
+        if (crop.interact) {
+          crop.interact(player)
+        } else if (crop.breakBuilding) {
+          crop.breakBuilding(player)
+        }
+      }
+    })
+  }
+}
+
+module.exports = Pitchfork

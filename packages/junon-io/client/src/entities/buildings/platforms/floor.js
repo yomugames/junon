448c8346b52c8cf717c692fdcc3f5e4ea9a1c86b
chickemeeple <rachaelcodetesting@gmail.com>
2025-08-14 15:21:58 -0600

    add perlin texture to texture list

--- packages/junon-io/client/src/entities/buildings/platforms/floor.js
+++ packages/junon-io/client/src/entities/buildings/platforms/floor.js
@@ -6,12 +6,16 @@ const perlin = require('perlin-noise');
 class Floor extends BaseFloor {
 
   getBaseSpritePath() {
+    return "solid_texture.png";
+  }
+
+  getTextureSpritePath() {
     if (this.data && this.data.textureIndex) {
       let textureName = this.game.floorTextures[this.data.textureIndex]
       return textureName
     }
 
-    return 'solid_texture.png'
+    return "solid_texture.png";
   }
 
   getBuildingSprite(x, y) {
@@ -96,18 +100,21 @@ class Floor extends BaseFloor {
 
     let sprite = new PIXI.Container()
 
-    this.baseSprite = new PIXI.Sprite(PIXI.utils.TextureCache[this.getBaseSpritePath()])
+    this.baseSprite = new PIXI.Sprite(PIXI.utils.TextureCache["solid_texture.png"])
     this.baseSprite.anchor.set(0.5)
 
     // this.texture = new PIXI.Sprite(PIXI.utils.TextureCache["noiseTexture" + String(Math.floor(Math.random() * 6)) + ".png"])
-    const baseTexture = PIXI.utils.TextureCache["floormap.png"]
+    const baseTexture = PIXI.utils.TextureCache[this.getTextureSpritePath()]
 
-    const chunk = new PIXI.Rectangle(x%(31*32),y%(31*32),32,32)
-    const texture = new PIXI.Texture(baseTexture, chunk)
 
-    this.texture = new PIXI.Sprite(texture)
-    this.texture.anchor.set(0.5)
-    this.texture.alpha = 0.2
+    if (this.getTextureSpritePath() === "floormap.png") {
+      const chunk = new PIXI.Rectangle(x % (31 * 32), y % (31 * 32), 32, 32)
+      const texture = new PIXI.Texture(baseTexture, chunk)
+      this.texture = new PIXI.Sprite(texture)
+      this.texture.alpha = 0.2
+    } else {
+      this.texture = new PIXI.Sprite(baseTexture)
+    }
 
     if (this.data.hasOwnProperty("colorIndex")) {
       let color = this.game.colors[this.data.colorIndex]
@@ -115,10 +122,13 @@ class Floor extends BaseFloor {
       this.texture.tint = color.value
     }
 
-    sprite.addChild(this.baseSprite)
+    this.texture.anchor.set(0.5)
+    
     this.baseSprite.addChild(this.texture)
-
+    sprite.addChild(this.baseSprite)
     return sprite;
+
+
   }
 
 

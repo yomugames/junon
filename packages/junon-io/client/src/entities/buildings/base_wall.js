3db4f8f439d4fea9f121b4ce5654ae50feca459d
chickemeeple <rachaelcodetesting@gmail.com>
2025-10-23 11:18:16 -0600

    set 3dwalls to default in prod

--- packages/junon-io/client/src/entities/buildings/base_wall.js
+++ packages/junon-io/client/src/entities/buildings/base_wall.js
@@ -117,6 +117,51 @@ class BaseWall extends BaseFloor {
     return sprite
   }
 
+layoutTile(tiles = this.getSides(), targetSprite = this.getTileSprite()) {
+  let down = tiles.down;
+  let up = tiles.up;
+  let left = tiles.left;
+  let right = tiles.right;
+  //there are 16 cases of up/down/right/left walls. Unfortunately a switch won't work here, and am not sure of a better way.
+  let texture = '';
+  if (!down && !up && !right && !left) {
+    texture = "wall0";
+  } else if (!left && !down && !right) {
+    texture = "wall0";
+  } else if (!up && !left && !down) {
+    texture = "wall1";
+  } else if (!left && !down) {
+    texture = "wall1";
+  } else if (!left && !up && !right) {
+    texture = "wall2";
+  } else if (!left && up && !right && down) {
+    texture = "wall2";
+  } else if (!left && !up && right && down) {
+    texture = "wall3";
+  } else if (!left && up && right && down) {
+    texture = "wall3";
+  } else if (!up && !right && !down) {
+    texture = "wall4";
+  } else if (!right && !down && left && up) {
+    texture = "wall4";
+  } else if (left && !up && right && !down) {
+    texture = "wall5";
+  } else if (left && up && right && !down) {
+    texture = "wall5";
+  } else if (left && !up && !right && down) {
+    texture = "wall6";
+  } else if (up && !right && down && left) {
+    texture = "wall6";
+  } else if (!up && right && down && left) {
+    texture = "wall7";
+  } else {
+    texture = "wall8";
+  }
+
+  targetSprite.texture = require('./wall3d').prototype.getTextures()[texture]
+  targetSprite.rotation = 0
+}
+
   getDefaultSpriteColor() {
     if (this.data.colorIndex) {
       let color = this.game.colors[this.data.colorIndex]
@@ -151,7 +196,51 @@ Object.assign(BaseWall.prototype, Tilable.prototype, {
   },
   getTileSprite() {
     return this.baseSprite
-  }
-})
+  },
+  layoutTile(tiles = this.getSides(), targetSprite = this.getTileSprite()) {
+    let down = tiles.down;
+    let up = tiles.up;
+    let left = tiles.left;
+    let right = tiles.right;
+    //there are 16 cases of up/down/right/left walls. Unfortunately a switch won't work here, and am not sure of a better way.
+    let texture = '';
+    if (!down && !up && !right && !left) {
+      texture = "wall0";
+    } else if (!left && !down && !right) {
+      texture = "wall0";
+    } else if (!up && !left && !down) {
+      texture = "wall1";
+    } else if (!left && !down) {
+      texture = "wall1";
+    } else if (!left && !up && !right) {
+      texture = "wall2";
+    } else if (!left && up && !right && down) {
+      texture = "wall2";
+    } else if (!left && !up && right && down) {
+      texture = "wall3";
+    } else if (!left && up && right && down) {
+      texture = "wall3";
+    } else if (!up && !right && !down) {
+      texture = "wall4";
+    } else if (!right && !down && left && up) {
+      texture = "wall4";
+    } else if (left && !up && right && !down) {
+      texture = "wall5";
+    } else if (left && up && right && !down) {
+      texture = "wall5";
+    } else if (left && !up && !right && down) {
+      texture = "wall6";
+    } else if (up && !right && down && left) {
+      texture = "wall6";
+    } else if (!up && right && down && left) {
+      texture = "wall7";
+    } else {
+      texture = "wall8";
+    }
 
-module.exports = BaseWall
+    targetSprite.texture = require('./wall3d').prototype.getTextures()[texture];
+    targetSprite.rotation = 0;
+  }
+});
+
+module.exports  =BaseWall;

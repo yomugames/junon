da6e5ad79d63166cfd5a6c0a1d2fbb60cac2c21e
kuroro <simple.yuji@gmail.com>
2025-11-07 13:45:22 -0500

    fix rsync command + custom matchmaker port

--- packages/junon-matchmaker/src/index.js
+++ packages/junon-matchmaker/src/index.js
@@ -53,7 +53,11 @@ function toArrayBuffer(buffer) {
 class MatchmakerServer {
   constructor() {
     this.bootTime = Date.now()
-    this.APP_SERVER_PORT = debugMode ? 3000 : 443
+    if (debugMode) {
+      this.APP_SERVER_PORT = 3000
+    } else {
+      this.APP_SERVER_PORT = process.env.MATCHMAKER_PORT || 443
+    }
     this.GAME_WEBSOCKET_SERVER_PORT = 2095
 
     this.ENVIRONMENT_LIST = ["vm", "development", "staging", "production"]

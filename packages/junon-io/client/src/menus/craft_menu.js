5b4f345923a9b722ca542bf27d995d54973459a2
wahlee <120862914+Wahlee1@users.noreply.github.com>
2026-06-23 11:59:49 -0500

    fix terrain tab being accessible for players that aren't sector owner

--- packages/junon-io/client/src/menus/craft_menu.js
+++ packages/junon-io/client/src/menus/craft_menu.js
@@ -247,8 +247,8 @@ class CraftMenu extends BaseMenu {
     let tabs = Array.from(this.el.querySelectorAll(".construction_tab"))
     if (tabs.length <= 1) return
 
-    // exclude terrain tab if not peaceful mode
-    if (!this.game.isPeaceful()) tabs.splice(0, 1)
+    // exclude terrain tab if not peaceful mode or sector owner
+    if (!this.game.isPeaceful() || !this.game.player.isSectorOwner()) tabs.splice(0, 1)
     
     let tabIndex = Array.prototype.indexOf.call(tabs, this.lastActiveTab)
     

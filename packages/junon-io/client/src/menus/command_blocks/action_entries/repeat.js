423ce12e5d5f8be8d6fc10ae529c654d14a6fe01
feilure <ethan.z.fei@gmail.com>
2026-07-30 18:23:43 -0700

    Functional Loop

--- packages/junon-io/client/src/menus/command_blocks/action_entries/repeat.js
+++ packages/junon-io/client/src/menus/command_blocks/action_entries/repeat.js
@@ -1,87 +1,29 @@
 const ActionEntry = require("./action_entry")
-const Helper = require("../../../../../common/helper")
 const ActionContainer = require("../action_container")
+const Helper = require("../../../../../common/helper")
+const SocketUtil = require("../../../util/socket_util")
 
 class Repeat extends ActionEntry {
 
   constructor(parent, data) {
     super(parent, data)
 
-    // handleActionValues() has already run, so times is available
-    this.redraw()
-
     this.el.querySelector(".add_action_btn")
       .addEventListener("click", this.onAddActionBtnClick.bind(this), true)
   }
 
+  static create(parent, data) {
+    if (!this.isValid(parent, data.actionKey)) return
+    return new Repeat(parent, data)
+  }
+
+  static isValid(parent, actionKey) {
+    return true
+  }
+
   handleActionValues(data) {
-    this.times = 5
-
+    this.times = data.times
     this.initActionContainer(data)
-  }
-
-  enterEditMode() {
-    let label = this.el.querySelector(".repeat_label")
-
-    let input = document.createElement("input")
-    input.type = "number"
-    input.min = 1
-    input.value = this.times
-    input.style.width = "50px"
-
-    label.replaceWith(input)
-
-    input.focus()
-    input.select()
-
-    const finish = () => {
-        this.times = Math.max(1, parseInt(input.value) || 1)
-        this.redraw()
-    }
-
-    input.addEventListener("blur", finish)
-
-    input.addEventListener("keydown", (e) => {
-        if (e.key === "Enter") {
-            finish()
-        }
-    })
-  }
-
-  onContainerClick(e) {
-    if (e.target.classList.contains("repeat_label")) {
-        this.enterEditMode()
-        return
-    }
-    if (e.target.classList.contains("delete_action_btn")) {
-      let id = parseInt(e.target.closest(".action_entry").dataset.id)
-      if (id !== this.id) return
-
-      if (this.isTempId()) {
-        this.remove()
-      } else {
-        this.submitDelete()
-      }
-    }
-  }
-
-  onAddActionBtnClick() {
-    this.game.commandBlockPicker.open({
-      mode: "actions",
-      parent: this
-    })
-  }
-
-  redraw() {
-    let old = this.el.querySelector(".repeat_label, input")
-
-    let label = document.createElement("div")
-    label.classList.add("action_key")
-    label.classList.add("repeat_label")
-    label.innerText =
-        `${Helper.capitalize(this.actionKey)} (${this.times}x)`
-
-    old.replaceWith(label)
   }
 
   createEl() {
@@ -90,10 +32,13 @@ class Repeat extends ActionEntry {
     row.classList.add("action_entry")
     row.classList.add("tab_1")
 
+    let deleteBtn = document.createElement("img")
+    deleteBtn.classList.add("delete_action_btn")
+    deleteBtn.src = "/assets/images/trash_icon.png"
+
     let actionKey = document.createElement("div")
     actionKey.classList.add("action_key")
-    actionKey.classList.add("repeat_label")
-    actionKey.innerText = `${Helper.capitalize(this.actionKey)} (${this.times}x)`
+    actionKey.innerText = Helper.capitalize(this.actionKey)
 
     let addActionBtn = document.createElement("img")
     addActionBtn.classList.add("add_action_btn")
@@ -107,18 +52,121 @@ class Repeat extends ActionEntry {
     actionValueList.classList.add("action_value_list")
     actionValue.appendChild(actionValueList)
 
-    let deleteBtn = document.createElement("img")
-    deleteBtn.classList.add("delete_action_btn")
-    deleteBtn.src = "/assets/images/trash_icon.png"
-
     row.appendChild(deleteBtn)
     row.appendChild(actionKey)
     row.appendChild(addActionBtn)
+    row.appendChild(this.createTimesContainer())
     row.appendChild(actionValue)
 
     this.parent.appendChildEl(row)
 
     return row
+  }
+
+  createTimesContainer() {
+    let container = document.createElement("div")
+    container.classList.add("repeat_times")
+
+
+    let input = document.createElement("input")
+    input.classList.add("repeat_times_input")
+    input.value = this.times || ""
+
+    input.addEventListener("keyup", this.onTimesInputKeyup.bind(this), true)
+    input.addEventListener("blur", this.onTimesInputBlur.bind(this), true)
+
+    let label2 = document.createElement("span")
+    label2.innerText = " times"
+
+    container.appendChild(input)
+    container.appendChild(label2)
+
+    return container
+  }
+
+  onContainerClick(e) {
+    if (e.target.classList.contains("delete_action_btn")) {
+      let id = parseInt(e.target.closest(".action_entry").dataset.id)
+      if (id !== this.id) return
+
+      if (this.isTempId()) {
+        this.remove()
+      } else {
+        this.submitDelete()
+      }
+    }
+
+    if (e.target.classList.contains("edit_action_value_btn")) {
+      let row = e.target.closest(".action_value_list_row")
+      this.enterEditMode(row)
+    }
+  }
+
+  enterEditMode(row) {
+    row.classList.add("edit_mode")
+    let input = row.querySelector("input")
+    input.focus()
+    input.select()
+  }
+
+  exitEditMode(row) {
+    row.classList.remove("edit_mode")
+  }
+
+  onTimesInputKeyup(e) {
+    if (e.which === 13) {
+      this.submitTimesChange(e.target.value)
+    } else if (e.which === 27) {
+      this.exitEditMode(e.target.closest(".action_value_list_row"))
+    }
+  }
+
+  onTimesInputBlur(e) {
+    this.submitTimesChange(e.target.value)
+  }
+
+  submitTimesChange(value) {
+    value = (value || "").trim()
+
+    if (value === "")
+      value = "1"
+
+    SocketUtil.emit("EditCommandBlock", {
+      operation: "edit",
+      id: this.id,
+      value: "times:" + value
+    })
+  }
+
+  edit(text) {
+    let tokens = text.split(":")
+
+    if (tokens[0] !== "times")
+      return
+
+    this.times = tokens[1]
+
+    let input = this.el.querySelector(".repeat_times input")
+    input.value = this.times || ""
+  }
+
+  onAddActionBtnClick() {
+    this.game.commandBlockPicker.open({
+      mode: "actions",
+      parent: this
+    })
+  }
+
+  editChildNode(data) {
+  }
+
+  toJson() {
+    return {
+      id: this.id,
+      actionKey: this.actionKey,
+      times: this.times,
+      actions: this.actions.map(action => action.toJson())
+    }
   }
 
 }
--- packages/junon-io/server/command_blocks/action_entries/repeat.js
+++ packages/junon-io/server/command_blocks/action_entries/repeat.js
@@ -1,11 +1,11 @@
 const ActionEntry = require("./action_entry")
 const ActionContainer = require("../action_container")
+const Constants = require("../../../common/constants.json")
 
 class Repeat extends ActionEntry {
 
   constructor(parent, data) {
     super(parent, data)
-    console.log("SERVER Repeat created", data)
   }
 
   static create(parent, data) {
@@ -18,9 +18,28 @@ class Repeat extends ActionEntry {
     return true
   }
 
+  static sanitizeTimes(times) {
+    let str = (times === undefined || times === null) ? "" : times.toString().trim()
+
+    if (str === "") return 1
+
+    if (/^-?\d+$/.test(str)) {
+      let num = parseInt(str)
+
+      if (isNaN(num) || num < 1) {
+        num = 1
+      } else if (num > Constants.maxRepeatTimes) {
+        num = Constants.maxRepeatTimes
+      }
+
+      return num
+    }
+
+    return str
+  }
+
   handleActionValues(data) {
-    /*this.times = data.times || 1*/
-    this.times = 5
+    this.times = Repeat.sanitizeTimes(data.times)
 
     this.initActionContainer(data)
   }
@@ -29,8 +48,24 @@ class Repeat extends ActionEntry {
 
   }
 
+  edit(value, player) {
+    let tokens = value.split(":")
+
+    if (tokens[0] !== "times")
+      return
+
+    this.times = Repeat.sanitizeTimes(tokens[1])
+
+    this.getSocketUtil().broadcast(this.game.getSocketIds(), "CommandBlockUpdated", {
+      id: this.id,
+      operation: "edit",
+      value: "times:" + this.times
+    })
+
+    this.commandBlock.onNodeChanged()
+  }
+
   toJson() {
-    console.log("Repeat.toJson()",this.times) //Temporary
     return {
       id: this.id,
       actionKey: this.actionKey,

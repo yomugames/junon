b567c0bb3896f1e43870901543a7dd8aa938632e
Jane Doe <kontrevolucia@gmail.com>
2026-08-13 13:07:01 +0400

    Removed backstab mechanic cuz kind of useless

--- packages/junon-io/server/entities/equipments/hand/base_knife.js
+++ packages/junon-io/server/entities/equipments/hand/base_knife.js
@@ -5,31 +5,6 @@ const Constants = require("../../../../common/constants.json")
 
 
 class BaseKnife extends MeleeEquipment {
-  getDamage(targetEntity) {
-    let damage = super.getDamage(targetEntity)
-    
-    if (!targetEntity) {
-      return damage
-    }
-    
-    const backstabDamage = this.getEquipmentBackstabDamage()
-    
-    const threshold = this.getConstants().stats.angleBackstabThreshold ?? 20
-    
-    const angle = this.owner.angle
-    const enemyAngle = targetEntity.angle
-    
-    let diff = angle - enemyAngle
-    while (diff > 180) diff -= 360
-    while (diff < -180) diff += 360
-    
-    if (Math.abs(diff) < threshold && !targetEntity.isBuilding()) {
-      damage = Math.floor(damage * backstabDamage)
-    }
-    
-    return damage
-  }
-  
   getEquipmentBackstabDamage() {
     if (this.sector) {
       if (this.sector.entityCustomStats[this.item.id] && typeof this.sector.entityCustomStats[this.item.id].backstabDamageMultiplier !== 'undefined') {

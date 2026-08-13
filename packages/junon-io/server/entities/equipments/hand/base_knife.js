73fc7e41468387a0c6da8606e9a6052dc1da1bd5
Jane Doe <kontrevolucia@gmail.com>
2026-08-13 13:07:25 +0400

    Removed getEquipmentBackstabDamage method

--- packages/junon-io/server/entities/equipments/hand/base_knife.js
+++ packages/junon-io/server/entities/equipments/hand/base_knife.js
@@ -5,18 +5,6 @@ const Constants = require("../../../../common/constants.json")
 
 
 class BaseKnife extends MeleeEquipment {
-  getEquipmentBackstabDamage() {
-    if (this.sector) {
-      if (this.sector.entityCustomStats[this.item.id] && typeof this.sector.entityCustomStats[this.item.id].backstabDamageMultiplier !== 'undefined') {
-        return this.sector.entityCustomStats[this.item.id].backstabDamageMultiplier
-      } else if (this.sector.itemCustomStats[this.type] && typeof this.sector.itemCustomStats[this.type].backstabDamageMultiplier !== 'undefined') {
-        return this.sector.itemCustomStats[this.type].backstabDamageMultiplier
-      }
-    }
-
-    return this.getConstants().stats.backstabDamageMultiplier
-  }
-  
   isKnife() {
     return true
   }
